500f6af9b8e781bf3fedbfe2d1ba53a8d55b64d6
Andreza Paiva <andrezasopaiva@gmail.com>
2023-02-10 14:32:38 -0300

    Initial Commit

---
+++ SunSafe/SunSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunSafeApp.swift
+//  SunSafe
+//
+//  Created by Andreza Paiva on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SunSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
